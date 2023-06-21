32caee17053149d6b30f024b7098977013a092e1
Sameer_Raturi <sameerraturi2001@gmail.com>
2023-06-21 22:51:14 +0530

    basic routing

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Hi');
+const express = require("express");
+
+// creating the server named as app
+const app = express();
+
+// basic routing
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+// app will be listening to all the requests coming at port 3000
+app.listen(3000);
