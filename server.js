2d8deea6f9bf8f1ed0d2e482d313f9dc87dc7bf0
Sameer_Raturi <sameerraturi2001@gmail.com>
2023-06-21 22:54:16 +0530

    sending data

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 // basic routing
 app.get("/", (req, res) => {
   res.send("Hi");
+  res.status(500).send("bye");
+  res.download("server.js");
 });
 
 // app will be listening to all the requests coming at port 3000
