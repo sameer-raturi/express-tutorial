1b80a683930c21d02c4d1b541e5faf1d37453cf0
Sameer_Raturi <sameerraturi2001@gmail.com>
2023-06-22 10:22:23 +0530

    rendering HTML

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require("express");
 // creating the server named as app
 const app = express();
 
+// setting view engine
+app.set("view engine", 'ejs');
+
 // basic routing
 app.get("/", (req, res) => {
-  res.send("Hi");
-  res.status(500).send("bye");
-  res.download("server.js");
+  res.render("index", { text12: 'Sameer'}); 
 });
 
 // app will be listening to all the requests coming at port 3000
